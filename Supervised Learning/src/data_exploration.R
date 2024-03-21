5f42b476fc286068899aac2caa8ef22a813bcd94
Mubarak Babslawal <mubarakbabslawal@gmail.com>
2024-03-21 11:43:28 +0100

    edit: updated code

--- Supervised Learning/src/data_exploration.R
+++ Supervised Learning/src/data_exploration.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(skimr)
 library(here)
+library(caret)
+library(corrplot)
 
 data <- read.csv(here("Supervised Learning/data", "Life_Expectancy_Data.csv"))
 skimr::skim_without_charts(data)
@@ -28,7 +30,7 @@ print(category_codes)
 
 #drop the region column
 data <- subset(data, select = -Region)
-data_2014 <- data %>% filter(Year == 2014) %>% select(-Year)
+data_2014 <- data %>% filter(Year == 2014) %>% select(-c(Year, Country))
 numeric_data_2014 <- data_2014 %>% select(-Country)
 
 #=============================================================================================================
@@ -42,24 +44,18 @@ str(data)
 skim_without_charts(data_2014)    
 
 
-# Install and load the corrplot package
-install.packages("corrplot")
-library(corrplot)
-
 # Compute the correlation matrix
 correlation_matrix <- cor(numeric_data_2014, use = "pairwise.complete.obs")
 
 # Plot the correlation matrix
 corrplot(correlation_matrix, method = "circle", type = "lower", order = "hclust", tl.col = "black", tl.srt = 45)
 
-file_path <- file.path(here("Supervised Learning/data"), "correlation_matrix.csv")
+#file_path <- file.path(here("Supervised Learning/data"), "correlation_matrix.csv")
 
 # Save the correlation matrix to a CSV file
-write.csv(correlation_matrix, file = file_path, row.names = TRUE)
+#write.csv(correlation_matrix, file = file_path, row.names = TRUE)
 
 #Plotting correlation between dependent variable and each independent variable
-
-
 # Calculate correlation coefficients between dependent variable and each independent variable
 correlation_with_dependent <- sapply(numeric_data_2014[, -which(names(numeric_data_2014) == "Life_expectancy")], function(x) cor(x, numeric_data_2014[["Life_expectancy"]]))
 
@@ -81,13 +77,90 @@ numeric_data_2014[, -which(names(numeric_data_2014) == "Life_expectancy")]
 subset(numeric_data_2014, select = -Life_expectancy)
 numeric_data_2014["Life_expectancy"]
 
-y <- numeric_data_2014[["Life_expectancy"]]
-x <- subset(numeric_data_2014, select = -Life_expectancy)
-scaled_x <- data.frame(scale(x))
+#Model 1. Using all data
+# Split data into training and testing sets (80% training, 20% testing)
+set.seed(123)  # Set seed for reproducibility
+train_index <- createDataPartition(numeric_data_2014$Life_expectancy, p = 0.8, list = FALSE)
+
+# Create training set
+train_data <- numeric_data_2014[train_index, ]
+y_train <- train_data[["Life_expectancy"]]
+x_train <- subset(train_data, select = -Life_expectancy)
+# Create testing set
+test_data <- numeric_data_2014[-train_index, ]
+y_test <- test_data[["Life_expectancy"]]
+x_test <- subset(test_data, select = -Life_expectancy)
 #run linear regression
-lm_model <- lm(y ~ ., data = x)
-scaled_model <- lm(y ~ ., data = scaled_x)
+lm_model <- lm(y_train ~ ., data = x_train)
+
 #try with sclaed variables and see if there's a difference
 # Print summary statistics
 summary(lm_model)
-summary(scaled_model)
+
+predictions <- predict(lm_model, newdata = test_data)
+mse <- mean((y_test - predictions)^2)
+rmse <- sqrt(mse)
+print(paste("Mean Squared Error (MSE):", mse))
+print(paste("Root Mean Squared Error (RMSE):", rmse))
+
+
+#Model 2: Removing columns uncorrelated with the dependent variable
+set.seed(123)  # Set seed for reproducibility
+uncorr = subset(numeric_data_2014, select = c(names(keep(correlation_with_dependent, ~ abs(.x) > 0.5)), "Life_expectancy"))
+train_index <- createDataPartition(uncorr$Life_expectancy, p = 0.8, list = FALSE)
+str(uncorr)
+
+# Create training set
+train_data <- uncorr[train_index, ]
+y_train <- train_data[["Life_expectancy"]]
+x_train <- subset(train_data, select = -Life_expectancy)
+# Create testing set
+test_data <- uncorr[-train_index, ]
+y_test <- test_data[["Life_expectancy"]]
+x_test <- subset(test_data, select = -Life_expectancy)
+#run linear regression
+lm_model <- lm(y_train ~ ., data = x_train)
+
+# Print summary statistics
+summary(lm_model)
+
+predictions <- predict(lm_model, newdata = test_data)
+mse <- mean((y_test - predictions)^2)
+rmse <- sqrt(mse)
+print(paste("Mean Squared Error (MSE):", mse))
+print(paste("Root Mean Squared Error (RMSE):", rmse))
+
+
+#Model 3: Use the backward feature selection directly in R
+
+# Assuming 'data' is your dataset and 'target_variable' is the name of your target variable
+
+set.seed(212)  # Set seed for reproducibility
+train_index <- createDataPartition(numeric_data_2014$Life_expectancy, p = 0.8, list = FALSE)
+
+# Create training set
+train_data <- numeric_data_2014[train_index, ]
+y_train <- train_data[["Life_expectancy"]]
+x_train <- subset(train_data, select = -Life_expectancy)
+# Create testing set
+test_data <- numeric_data_2014[-train_index, ]
+y_test <- test_data[["Life_expectancy"]]
+x_test <- subset(test_data, select = -Life_expectancy)
+#run linear regression
+full_model <- lm(y_train ~ ., data = x_train)
+
+# Perform backward feature selection using AIC
+backward_model <- step(full_model, direction = "backward")
+
+# Print summary of the final model
+summary(backward_model)
+
+predictions <- predict(lm_model, newdata = test_data)
+mse <- mean((y_test - predictions)^2)
+rmse <- sqrt(mse)
+r_squared <- R2(y_test, predictions)
+print(paste("Mean Squared Error (MSE):", mse))
+print(paste("Root Mean Squared Error (RMSE):", rmse))
+print(paste("R-squared:", r_squared))
+
+numeric_data_2014 %>% filter(Life_expectancy > 0) %>% count()
